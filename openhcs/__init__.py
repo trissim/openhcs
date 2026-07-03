d9e33c51d7ef088ee0b70f01f033d7a39a16950c
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-07-03 19:43:35 -0400

    bump version to 0.5.18

--- openhcs/__init__.py
+++ openhcs/__init__.py
@@ -13,7 +13,7 @@ import sys
 import platform
 from pathlib import Path
 
-__version__ = "0.5.17"
+__version__ = "0.5.18"
 
 # Configure polystore defaults for OpenHCS integration
 os.environ.setdefault("POLYSTORE_METADATA_FILENAME", "openhcs_metadata.json")
