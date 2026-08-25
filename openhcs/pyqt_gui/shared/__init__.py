f2d54baaef7c09ad76b09b46aac3b06c051a8a71
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-08-25 16:54:45 -0400

    Remove unreachable GUI scaffolding

--- openhcs/pyqt_gui/shared/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Shared theming declarations for the OpenHCS PyQt application."""
-
-from pyqt_reactive.theming import ColorScheme
-from pyqt_reactive.theming import PaletteManager, ThemeManager
-from openhcs.pyqt_gui.shared.config_validator import ColorSchemeValidator
-
-__all__ = ["ColorScheme", "PaletteManager", "ThemeManager", "ColorSchemeValidator"]
